11673b465b73f038cb4085bda144d188f9bec298
Gustavo Daneluti <gdaneluti@e-safer.com.br>
2019-04-26 09:18:24 -0300

    Horarios disponiveis

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
 const AppointmentController = require('./app/controllers/AppointmentController.js')
+const AvailableController = require('./app/controllers/AvailableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -32,4 +33,5 @@ routes.get('/app/dashboard', DashboardController.index)
 
 routes.get('/app/appointments/new/:provider', AppointmentController.create)
 
+routes.get('/app/available/:provider', AvailableController.index)
 module.exports = routes
